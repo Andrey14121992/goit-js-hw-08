bdfce95d2b79b51652ba48ae84e729fc3ed5dfda
Andrey14121992 <andrey1477@ukr.net>
2022-10-02 22:10:31 +0300

    Перед здачей дз

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 console.log(galleryItems);
+
+const galaryDiv = document.querySelector('.gallery');
+
+const createGalery = collection => {
+    collection.map(({ preview, original, description }) => {
+        const markingElement = `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`;
+        galaryDiv.insertAdjacentHTML('afterbegin', markingElement);
+    })
+}
+
+createGalery(galleryItems)
+
+
+const onImgClick = event => {
+    event.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt',captionDelay:250});
+}
+galaryDiv.addEventListener('click', onImgClick);
